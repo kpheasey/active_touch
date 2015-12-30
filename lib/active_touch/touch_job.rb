6ca97b72eb93928b38cae95d1ba284b108ab48b6
Kevin Pheasey <kevin@kpheasey.com>
2015-12-30 10:07:33 -0500

    Retry deadlocks up to 3 times with a randomized pause between each retry.

--- lib/active_touch/touch_job.rb
+++ lib/active_touch/touch_job.rb
@@ -13,7 +13,26 @@ module ActiveTouch
         associated.send(after_touch) unless after_touch.blank?
 
       elsif !associated.nil?
-        associated.update_all(updated_at: record.updated_at)
+
+        # attempt to update all records 3 times.  Dead locks are common when
+        # multiple related touches are triggered at the same time.  A randomized
+        # delay of up to 3 seconds between retries is used to prevent the error from
+        # being raised
+        begin
+          retries ||= 0
+          associated.update_all(updated_at: record.updated_at)
+
+        rescue ActiveRecord::StatementInvalid
+          if retries < 3
+            sleep(rand(3) + 1)
+            retries += 1
+            retry
+          else
+            raise
+          end
+        end
+
+
         associated.each { |associate| associate.send(after_touch) } unless after_touch.blank?
       end
     end
