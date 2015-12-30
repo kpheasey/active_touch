df91208eb2f8442e9b41156967f5591a097ba4e8
Kevin Pheasey <kevin@kpheasey.com>
2015-12-30 11:04:45 -0500

    Add default is_async value

--- lib/active_touch/touch_job.rb
+++ lib/active_touch/touch_job.rb
@@ -1,27 +1,27 @@
 module ActiveTouch
   class TouchJob < ActiveJob::Base
 
-    def perform(record, association, after_touch, is_async)
-      if association == 'self'
-        associated = record
-      else
-        associated = record.send(association)
-      end
+    def perform(record, association, after_touch, is_async = false)
+      associated = association == 'self' ? record : record.send(association)
 
       if associated.is_a? ActiveRecord::Base
         associated.update_columns(updated_at: record.updated_at)
         associated.send(after_touch) unless after_touch.blank?
 
       elsif !associated.nil?
-        associated.each do |associate|
 
-          if is_async
+        if is_async
+          # create a separate touch job for each associated record.
+          # this helps avoid deadlocks
+          associated.each do |associate|
             TouchJob
                 .set(queue: ActiveTouch.configuration.queue)
                 .perform_later(associate, 'self', after_touch, is_async)
-          else
-            TouchJob.perform_later(associate, 'self', after_touch, is_async)
           end
+
+        else
+          associated.update_all(updated_at: record.updated_at)
+          associated.each { |associate| associate.send(after_touch) } unless after_touch.blank?
         end
       end
     end
